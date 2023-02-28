936669cab60e71fadddc14f6778742e0d680246c
Aidan A <84807134+Aidan-A-2008@users.noreply.github.com>
2023-02-28 17:23:24 -0600

    Update send.php

--- send.php
+++ send.php
@@ -5,9 +5,9 @@
  $name= $_REQUEST['name'];
  $email = $_REQUEST['Email'];
  $message = $_REQUEST['Message'];
- $subject = "Message from Contact form !";
+ $subject = "Contact | aidan-a.me";
 
- $to ="xxxxxxxxxxxxx@gmail.com";  // change receiving email id 
+ $to ="contact@aidan-a.me";  // change receiving email id 
  
  $content = "Name : ". $name. "\r\nContact email : ". $email. "\r\n \r\nMessage : \r\n \r\n".$message ; // name [break] email [break] message
  
